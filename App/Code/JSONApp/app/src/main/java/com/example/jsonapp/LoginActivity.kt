0632ea97e96cfa0a89ac19f9167ba4b2712e2b96
jaimegr23 <garridora@esat-alumni.com>
2023-12-11 02:07:47 +0100

    login completed

---
+++ App/Code/JSONApp/app/src/main/java/com/example/jsonapp/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.jsonapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val editTextUsername = findViewById<EditText>(R.id.UserText)
+        val editTextPassword = findViewById<EditText>(R.id.PasswordText)
+        val buttonLogin = findViewById<Button>(R.id.LoginButton)
+
+        buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+
+            // login logic
+            if ((username == "ruben" && password == "123456*") || (username == "jaime" && password == "jaime123a")) {
+                Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MyMainActivityFinal::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "User or password are not correct", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
